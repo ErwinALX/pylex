bfcb0a3c9beedb5fa8eb836c1ff2fdc0dc4acec9
Sebastian Garrido <sebasgarcep@gmail.com>
2017-09-12 13:41:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python Lexer
+
+## Installation
+
+In a Debian/Ubuntu distro:
+
+```bash
+sudo apt-get install flex
+```
+
+## Usage
+
+Compilation:
+
+```bash
+./compile.sh
+```
+
+Running:
+
+```bash
+```
---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+lex --outfile=build/LAB01_Bustillo_Garrido.c src/LAB01_Bustillo_Garrido.l && \
+gcc build/LAB01_Bustillo_Garrido.c -o build/LAB01_Bustillo_Garrido.out -ll
