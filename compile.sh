facaabc078cfa24c4870bae6290182c6460cbfde
Erwin Bustillo <erwinb1996@gmail.com>
2017-09-18 15:27:41 +0200

    adds scripts

--- compile.sh
+++ compile.sh
@@ -3,5 +3,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+mkdir build
+
 lex --outfile=build/LAB01_Bustillo_Garrido.c src/LAB01_Bustillo_Garrido.l && \
 gcc build/LAB01_Bustillo_Garrido.c -o build/LAB01_Bustillo_Garrido.out -ll
--- output.sh
+++ output.sh
@@ -3,4 +3,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-./compile.sh && cat "$1" | ./build/LAB01_Bustillo_Garrido.out
+./run.sh "$1" > "$2"
