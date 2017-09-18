4f49eecff6175d006460451e495fdc6130dcc135
Sebastian Garrido <sebasgarcep@gmail.com>
2017-09-18 00:46:16 -0500

    feat: solved details

--- README.md
+++ README.md
@@ -19,4 +19,5 @@ Compilation:
 Running:
 
 ```bash
+./run.sh
 ```
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-./compile.sh && cat data/simple.py | ./build/LAB01_Bustillo_Garrido.out
+./compile.sh && cat data/example.py | ./build/LAB01_Bustillo_Garrido.out
